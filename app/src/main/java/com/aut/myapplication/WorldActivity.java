9a4a39fe3810a4630858daf956cea72a617dc88b
Joan <jtpayoran@gmail.com>
2020-11-01 08:50:56 +1300

    fix layout

---
+++ app/src/main/java/com/aut/myapplication/WorldActivity.java
@@ -0,0 +1,14 @@
+package com.aut.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WorldActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_world);
+    }
+}
